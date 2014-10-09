44c97b98b93b13a2812b31001ca2432e13a1a3c2
Tarun Pondicherry <tarunpondicherry@users.noreply.github.com>
2014-10-09 13:45:20 -0700

    Update LightUpAllOnTest.ino

--- arduino-plugin-test/LightUpAllOnTest/LightUpAllOnTest.ino
+++ arduino-plugin-test/LightUpAllOnTest/LightUpAllOnTest.ino
@@ -13,6 +13,12 @@ void allOn() {
   on(_LU_INPUT_SIGNAL_4);
   on(_LU_INPUT_SIGNAL_5);
   on(_LU_INPUT_SIGNAL_6);
+  on(INPUT_1);
+  on(INPUT_2);
+  on(INPUT_3);
+  on(INPUT_4);
+  on(INPUT_5);
+  on(INPUT_6);
 }
 
 void allOff() {
@@ -28,11 +34,26 @@ void allOff() {
   off(_LU_INPUT_SIGNAL_4);
   off(_LU_INPUT_SIGNAL_5);
   off(_LU_INPUT_SIGNAL_6);
+  off(INPUT_1);
+  off(INPUT_2);
+  off(INPUT_3);
+  off(INPUT_4);
+  off(INPUT_5);
+  off(INPUT_6);
 }
 
 
 void setup() {
   disableInputLights();
+  
+  pinMode(INPUT_1, OUTPUT);
+  pinMode(INPUT_2, OUTPUT);
+  pinMode(INPUT_3, OUTPUT);
+  pinMode(INPUT_4, OUTPUT);
+  pinMode(INPUT_5, OUTPUT);
+  pinMode(INPUT_6, OUTPUT);
+
+  
   allOn();
 }
 
